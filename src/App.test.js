4b7c334f33131f67cc42dc46fde84bc831c3440d
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2023-07-24 11:59:31 -0600

    to equals

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { calculateResult } from './problems/Problem1';
 
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(calculateResult([9 ,6 ,4 ,1 ,5 ,8 ])).toBe([2 ,3 ,7 ]);
+test('array [9 ,6 ,4 ,1 ,5 ,8 ] equals to [2 ,3 ,7 ]', () => {
+  expect(calculateResult([9 ,6 ,4 ,1 ,5 ,8 ])).toEqual([2 ,3 ,7 ]);
 });
